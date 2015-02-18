8adc669d6b1ab9bffb010da6318935524bc36a62
Rene Bentes Pinto <renebentes@yahoo.com.br>
2015-02-18 17:59:20 -0300

    Reorganização

---
+++ TWP367.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from random import randint
+
+print('Bem vindo!')
+
+sorteado = randint(1, 100)
+chute = 0
+
+while chute != sorteado:
+    chute = int(input("Chute um número: "))
+    if chute == sorteado:
+        print("Você venceu!")
+    else:
+        if chute > sorteado:
+            print('Alto')
+        else:
+            print('Baixo')
+
+print('Fim de Jogo')
